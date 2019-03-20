87b0e664a89b95ed1f61c7d62d33b27c14b6d935
DAVID EDUARDO SAAVEDRA HERNANDEZ <de.saavedra@sis.virtual.uniandes.edu.co>
2019-03-20 18:09:50 -0500

    Primer Commit

---
+++ virtualEnvs/djangodev/Scripts/django-admin.py
@@ -0,0 +1,7 @@
+#!c:\users\estudiante\pictures\proyecto\virtualenvs\djangodev\scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.0.dev20190320183043','django-admin.py'
+__requires__ = 'Django==3.0.dev20190320183043'
+__import__('pkg_resources').require('Django==3.0.dev20190320183043')
+__file__ = 'C:\\Users\\estudiante\\Pictures\\django\\django\\bin\\django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
